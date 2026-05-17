ddf78a0d11aafde6a12d1f232d4c932137112608
Ege Kutan <ege.kutan@gmail.com>
2026-05-17 14:42:16 +0300

    I have completed cell class and tickable interface.

---
+++ src/core/Cell.java
@@ -0,0 +1,8 @@
+package core;
+
+public abstract class Cell {
+    protected int x;//for the x coordinate
+    protected int y;//for the y coordinate
+    protected char s;
+
+}
---
+++ src/core/Tickable.java
@@ -0,0 +1,5 @@
+package core;
+
+public interface Tickable {
+    void updateStatus();
+}
