5a3f7cf4ceb95cd4190a3ff60395ae7dcd8e03c4
denizelmali <denizelmaligithub@gmail.com>
2026-05-23 09:54:15 +0300

    I started the utility distributor class and the foundation of the BFS algorithm was laid.

---
+++ src/UtilityDistributor.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class UtilityDistributor {
+    static class BFSNode{
+        int row;
+        int col;
+        int remainingCapacity;
+
+        public BFSNode(int row, int col, int remainingCapacity) {
+            this.row = row;
+            this.col = col;
+            this.remainingCapacity = remainingCapacity;
+        }
+    }
+    public static void distribute(Cell[][] grid, int startRow, int startCol,int initialCapacity) {
+        int rowCount = grid.length;
+        int colCount = grid[0].length;
+
+        boolean[][] visited = new boolean[rowCount][colCount];
+        Queue<BFSNode> queue = new LinkedList<>();
+
+        queue.add(new BFSNode(startRow, startCol, initialCapacity));
+        visited[startRow][startCol] = true;
+        int[] dRow = {-1, 1, 0, 0};
+        int[] dCol = {0, 0, -1, 1};
+        while (!queue.isEmpty()) {
+        BFSNode current = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int newRow = current.row + dRow[i];
+                int newCol = current.col + dCol[i];
+                if (newRow >= 0 && newRow < rowCount && newCol >= 0 && newCol < colCount){
+                    if (!visited[newRow][newCol]) {
+                        Cell neighborCell = grid[newRow][newCol];
+                    }
+                }
+            }
+        }
+    }
+
+}
+
